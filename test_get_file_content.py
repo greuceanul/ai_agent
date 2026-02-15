54019e5fcdfec1508a16502ecd829217271f2e74
darth-kratos <catalin.tucra@gmail.com>
2026-02-15 10:58:57 +0200

    tested text reading functionality

---
+++ test_get_file_content.py
@@ -0,0 +1,28 @@
+from functions.get_file_content import get_file_content
+
+
+def test():
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for current file:")
+    print(result)
+    print("")
+
+    result = get_file_content("calculator", "main.py")
+    print("Result for 'main.py':")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for 'calculator.py':")
+    print(result)
+
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for '/bin/cat':")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    print("Result for 'does_not_exist.py':")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
